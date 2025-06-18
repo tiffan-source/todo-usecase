753d3e041881d09ac5ec580cffc72fd255614866
tiffan-source <zohoungbogboanlyou@gmail.com>
2025-06-18 12:53:08 +0200

    chore: correct presenter and add generic type

--- src/common/presenters/presenter.ts
+++ src/common/presenters/presenter.ts
@@ -1,8 +1,8 @@
 import type { outputDto } from "common/dtos/outputDto.js";
 
-export interface IPresenter {
+export interface IPresenter<I> {
   // The presenter interface defines the contract for presenting output data.
   // It should be implemented by classes that handle the presentation logic.
 
-  present(output: outputDto): void; // Method to present the output data
+  present(output: outputDto<I>): void; // Method to present the output data
 }
