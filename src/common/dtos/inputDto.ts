6a6d2b6638f75b9e95d64afa71c9059e6f340c05
tiffan-source <zohoungbogboanlyou@gmail.com>
2025-06-18 22:32:54 +0200

    fix: timestamp must be mandatory

--- src/common/dtos/inputDto.ts
+++ src/common/dtos/inputDto.ts
@@ -1,6 +1,6 @@
 export interface inputDto<I> {
   // This interface can be extended to include common properties for input DTOs
   // For example, you might want to include a timestamp
-  timestamp?: string | Date;
+  timestamp: string | Date;
   input: I; // The actual input data for the use case:
 }
